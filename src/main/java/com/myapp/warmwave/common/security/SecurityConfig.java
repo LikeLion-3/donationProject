69d59f679a72daeed8013f4d7ac8549af26a94e1
havegrit <shin_jaean@naver.com>
2023-11-24 15:57:55 +0900

    style: Optimize imports

--- src/main/java/com/myapp/warmwave/common/security/SecurityConfig.java
+++ src/main/java/com/myapp/warmwave/common/security/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.myapp.warmwave.common.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -20,6 +19,7 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 @RequiredArgsConstructor
 public class SecurityConfig {
     private final CorsFilter corsFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
