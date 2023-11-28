7e60fa9200bfb97b2624a23676a1ea0d97f09a50
WOOK0112 <dong930112@gmail.com>
2023-11-28 14:16:49 +0900

    refactor: 게시글 이미지 업로드 내부로직 개선

--- src/main/java/com/myapp/warmwave/domain/image/service/ImageService.java
+++ src/main/java/com/myapp/warmwave/domain/image/service/ImageService.java
@@ -3,12 +3,14 @@ package com.myapp.warmwave.domain.image.service;
 import com.myapp.warmwave.domain.article.entity.Article;
 import com.myapp.warmwave.domain.image.entity.Image;
 import com.myapp.warmwave.domain.image.repository.ImageRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -18,16 +20,13 @@ import java.io.IOException;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class ImageService {
 
 @Value("${image.upload.path}")
-private String imageStorePath = "/Users/wook/upload/article-images";
+private String imageStorePath;
 
-    private ImageRepository imageRepository;
-
-    public ImageService(ImageRepository imageRepository) {
-        this.imageRepository = imageRepository;
-    }
+    private final ImageRepository imageRepository;
 
     public List<Image> uploadImages(Article article, List<MultipartFile> imageFiles) throws IOException {
         List<Image> images = new ArrayList<>();
@@ -41,13 +40,11 @@ private String imageStorePath = "/Users/wook/upload/article-images";
         }
 
         for (MultipartFile imageFile : imageFiles) {
-
             String originalFilename = imageFile.getOriginalFilename();
             String fileExtension = originalFilename.substring(originalFilename.lastIndexOf(".") + 1);
-            String fileName = UUID.randomUUID().toString() + "." + fileExtension;
-            String filePath = imageStorePath + File.separator + fileName;
+            String fileName = UUID.randomUUID() + "." + fileExtension;
 
-            imageFile.transferTo(new File(filePath));
+            imageFile.transferTo(Paths.get(imageStorePath).resolve(fileName));
 
             String imageUrl = ServletUriComponentsBuilder.fromCurrentContextPath()
                     .path("/images/")
