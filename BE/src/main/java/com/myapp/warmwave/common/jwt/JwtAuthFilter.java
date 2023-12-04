faf25d9e7acf5061bf4cff114b7aa298e5f3e560
havegrit <shin_jaean@naver.com>
2023-12-04 10:55:22 +0900

    Fix: UserDetailsService -> CustomUserDetailsService

--- BE/src/main/java/com/myapp/warmwave/common/jwt/JwtAuthFilter.java
+++ BE/src/main/java/com/myapp/warmwave/common/jwt/JwtAuthFilter.java
@@ -1,5 +1,6 @@
 package com.myapp.warmwave.common.jwt;
 
+import com.myapp.warmwave.config.oauth.service.CustomUserDetailsService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -8,7 +9,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -21,7 +21,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 
     private final JwtProvider jwtProvider;
 
-    private final UserDetailsService userDetailsService;
+    private final CustomUserDetailsService userDetailsService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
