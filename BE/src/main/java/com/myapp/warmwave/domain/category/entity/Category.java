59115c5bcf00103eb77a58a334cf0e5043c701cd
WOOK0112 <dong930112@gmail.com>
2023-12-06 16:45:26 +0900

    feat: 카테고리 init, 추가, 조회, 전체조회 기능 추가 -init: 애플리케이션 시작시 기본카테고리 자동추가 기능

--- BE/src/main/java/com/myapp/warmwave/domain/category/entity/Category.java
+++ BE/src/main/java/com/myapp/warmwave/domain/category/entity/Category.java
@@ -15,8 +15,4 @@ public class Category {
     private Long id;
 
     private String name;
-
-    public Category(String name) {
-        this.name = name;
-    }
 }
--- BE/src/main/java/com/myapp/warmwave/domain/category/service/CategoryService.java
+++ BE/src/main/java/com/myapp/warmwave/domain/category/service/CategoryService.java
@@ -1,15 +1,24 @@
 package com.myapp.warmwave.domain.category.service;
 
+import com.myapp.warmwave.common.exception.CustomException;
 import com.myapp.warmwave.domain.category.dto.CategoryAllPostDto;
+import com.myapp.warmwave.domain.category.dto.CategoryDto;
 import com.myapp.warmwave.domain.category.entity.Category;
 import com.myapp.warmwave.domain.category.repository.CategoryRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
+
+import static com.myapp.warmwave.common.exception.CustomExceptionCode.ALREADY_EXIST_CATEGORY;
+import static com.myapp.warmwave.common.exception.CustomExceptionCode.NOT_FOUND_CATEGORY;
 
 @Service
 @RequiredArgsConstructor
@@ -17,23 +26,56 @@ import java.util.stream.Collectors;
 public class CategoryService {
     private final CategoryRepository categoryRepository;
 
-    public Category createCategory(Category category) {
+    public Category createCategory(CategoryDto dto) {
+        Category category = Category.builder()
+                .name(dto.getName())
+                .build();
+
+        Optional.ofNullable(categoryRepository.findByName(category.getName()))
+                .ifPresent(existingCategory -> {
+                    throw new CustomException(ALREADY_EXIST_CATEGORY);
+                });
+
         return categoryRepository.save(category);
     }
 
-    public List<Category> createAllCategory(CategoryAllPostDto categoryAllPostDto) {
-        List<Category> categories = categoryAllPostDto.getNames()
-                .stream()
-                .map(name -> new Category(name))
-                .collect(Collectors.toList());
+    public void createBasicCategories(CategoryAllPostDto categoryAllPostDto) {
+        List<String> strCategories = parseCategoryStrToArray(categoryAllPostDto.getCategoryNames());
 
-        return categoryRepository.saveAll(categories);
+        List<Category> categories = strCategories.stream()
+                .filter(strCategory -> categoryRepository.findByName(strCategory) == null)
+                .map(strCategory -> Category.builder().name(strCategory).build())
+                .toList();
+
+        if(!categories.isEmpty()) {
+            categoryRepository.saveAll(categories);
+        }
     }
 
 
-    public Optional<Category> getOne(long articleId) {
-        return categoryRepository.findById(articleId);
+    public List<Category> getCategory(CategoryDto dto) {
+        List<Category> categoryList = new ArrayList<>();
 
+        for (String category : parseCategoryStrToArray(dto.getName())) {
+            categoryList.add(validateCategory(category));
+        }
 
+        return categoryList;
+    }
+
+    public Page<Category> getAllCategory() {
+        List<Category> allCategories = categoryRepository.findAll();
+
+        return new PageImpl<>(allCategories, PageRequest.of(1, allCategories.size()), allCategories.size());
+    }
+    private List<String> parseCategoryStrToArray(String input) {
+        return Arrays.stream(input.split(",\\s*"))
+                .map(String::trim)
+                .toList();
+    }
+
+    private Category validateCategory(String categoryName) {
+        return Optional.ofNullable(categoryRepository.findByName(categoryName))
+                .orElseThrow(() -> new CustomException(NOT_FOUND_CATEGORY));
     }
 }
