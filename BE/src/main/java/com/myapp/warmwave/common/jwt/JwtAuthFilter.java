7ab3651cbc0ee2798c4aef1816aa6d2fb5da61e1
havegrit <shin_jaean@naver.com>
2023-12-18 01:29:56 +0900

    Refactor: 필터 단에서 쿠키가 만료되었을 때 리프레시 토큰을 통하여 액세스 토큰 재발급 하도록 작업

--- BE/src/main/java/com/myapp/warmwave/common/jwt/JwtAuthFilter.java
+++ BE/src/main/java/com/myapp/warmwave/common/jwt/JwtAuthFilter.java
@@ -1,13 +1,14 @@
 package com.myapp.warmwave.common.jwt;
 
+import com.myapp.warmwave.common.exception.CustomException;
+import com.myapp.warmwave.common.jwt.service.JwtRefreshService;
 import com.myapp.warmwave.config.oauth.service.CustomUserDetailsService;
+import com.myapp.warmwave.config.security.CookieManager;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -20,42 +21,47 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static com.myapp.warmwave.common.exception.CustomExceptionCode.EXPIRED_JWT;
+import static com.myapp.warmwave.config.security.CookieManager.ACCESS_TOKEN;
+import static com.myapp.warmwave.config.security.CookieManager.REFRESH_TOKEN;
 
 @Component
 @RequiredArgsConstructor
 public class JwtAuthFilter extends OncePerRequestFilter {
     private final JwtProvider jwtProvider;
-
     private final CustomUserDetailsService userDetailsService;
+    private final CookieManager cookieManager;
+    private final JwtRefreshService jwtRefreshService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String token = null;
+        String token = cookieManager.getCookie(request, ACCESS_TOKEN);
         String email = null;
-        Cookie[] cookies = request.getCookies();
 
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("accessToken")) {
-                    token = cookie.getValue();
-                    if (!jwtProvider.isTokenValid(token)) {
-                        response.setStatus(HttpStatus.UNAUTHORIZED.value());
-                        response.getWriter().write(EXPIRED_JWT.getMessage());
-                        return;
-                    }
-                    Map<String, Object> claims = (HashMap<String, Object>) jwtProvider.getClaims(token).get("body");
-                    email = claims.get("email").toString();
-                    if (email != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-                        UserDetails userDetails = userDetailsService.loadUserByUsername(email);
-                        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-                        authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                        SecurityContextHolder.getContext().setAuthentication(authToken);
-                    }
-                }
+        if (token == null) {
+            System.out.println("token is not exists in cookies");
+            return;
+        }
+
+        try {
+            jwtProvider.isTokenExpired(token);
+        } catch (CustomException e) {
+            //토큰이 파기되었을 때 리프레시 토큰을 통해 액세스 토큰을 재발급
+            if (e.getExceptionCode().equals(EXPIRED_JWT)) {
+                Map<String, String> tokens = jwtRefreshService.refreshToken(request);
+                cookieManager.setCookie(response, ACCESS_TOKEN, tokens.get(ACCESS_TOKEN), jwtProvider.getAccessTokenExpirationPeriod());
+                cookieManager.setCookie(response, REFRESH_TOKEN, tokens.get(REFRESH_TOKEN), jwtProvider.getRefreshTokenExpirationPeriod());
+
+                token = cookieManager.getCookie(request, ACCESS_TOKEN);
             }
-        } else {
-            //확인용
-            System.out.println("cookie does not exists !!!");
+        }
+
+        Map<String, Object> claims = (HashMap<String, Object>) jwtProvider.getClaims(token).get("body");
+        email = claims.get("email").toString();
+        if (email != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = userDetailsService.loadUserByUsername(email);
+            UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+            authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            SecurityContextHolder.getContext().setAuthentication(authToken);
         }
 
         filterChain.doFilter(request, response);
